30660b8dedfe28868a3388d087689551c6a4e17e
HitG010 <hiteshgupta_23ec091@dtu.ac.in>
2024-08-29 12:41:03 +0530

    Preprocess Changes

--- preprocess.py
+++ preprocess.py
@@ -45,21 +45,44 @@ def preprocessing(image, threshold=10):
     
     return img, dct_filtered, laplacian
 
-
-# image_path = 'test.jpg'
+def dct_YCbCr(image, threshold=10):
+    """
+    Applies a high-pass filter to the DCT coefficients.
+    
+    Parameters:
+    dct_coeffs (numpy.ndarray): DCT coefficients of the image.
+    threshold (int): Threshold for high-pass filtering.
+    
+    Returns:
+    numpy.ndarray: High-pass filtered DCT coefficients.
+    """
+    # Zero out low-frequency components below the threshold
+    img = cv2.imread(image, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (224, 224))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    Y, Cr, Cb = cv2.split(img)
+    dct_Y = dct(dct(Y.T, norm='ortho').T, norm='ortho')
+    dct_Y = high_pass_filter(dct_Y, threshold = threshold)
+    dct_Cr = dct(dct(Cr.T, norm='ortho').T, norm='ortho')
+    # dct_Cr = high_pass_filter(dct_Cr, threshold = threshold)
+    dct_Cb = dct(dct(Cb.T, norm='ortho').T, norm='ortho')
+    dct_Cb = high_pass_filter(dct_Cb, threshold = threshold)
+    return dct_Y, img, dct_Cr, dct_Cb
+image_path = 'test.jpg'
+dct_Y, img, dct_Cr, dct_Cb = dct_YCbCr(image_path)
 # dct_filtered, laplacian = preprocessing(image_path)
 # print(dct_filtered.shape)
 # print(laplacian.shape)
 
-# # Show the images
-# import matplotlib.pyplot as plt
-# plt.figure(figsize=(10, 5))
-# plt.subplot(1, 2, 1)
-# plt.imshow(dct_filtered, cmap='gray')
-# plt.title('DCT Filtered Image')
-# plt.axis('off')
-# plt.subplot(1, 2, 2)
-# plt.imshow(laplacian, cmap='gray')
-# plt.title('Laplacian Filtered Image')
-# plt.axis('off')
-# plt.show()
+# Show the images
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(dct_Cr, cmap='gray')
+plt.title('DCT Filtered Image')
+plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.imshow(img)
+plt.title('Laplacian Filtered Image')
+plt.axis('off')
+plt.show()
